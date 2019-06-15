6336708580990ad62095395524553203a4317a40
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-06-15 21:02:37 +0200

    -added migration to rename credential table to specialist

---
+++ src/MedPark.MedicalPractice/Migrations/20190615165427_specialistTableupdate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.MedicalPractice.Migrations
+{
+    public partial class specialistTableupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Credential");
+
+            migrationBuilder.CreateTable(
+                name: "Specialist",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Cellphone = table.Column<string>(nullable: true),
+                    Avatar = table.Column<string>(nullable: true),
+                    SpecialityId = table.Column<Guid>(nullable: true),
+                    PracticeId = table.Column<Guid>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    IsVerfied = table.Column<bool>(nullable: false),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialist", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Specialist");
+
+            migrationBuilder.CreateTable(
+                name: "Credential",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Avatar = table.Column<string>(nullable: true),
+                    Cellphone = table.Column<string>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    IsVerfied = table.Column<bool>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    PracticeId = table.Column<Guid>(nullable: false),
+                    SpecialityId = table.Column<Guid>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credential", x => x.Id);
+                });
+        }
+    }
+}
