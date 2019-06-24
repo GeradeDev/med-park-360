1ac0f48f98419e79c5913bcbc748767ebcfa1223
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-06-24 19:22:32 +0200

    -added generic IRepo interface

---
+++ src/MedPark.Common/IMedParkRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedPark.Common
+{
+    public interface IMedParkRepository<TEntity> where TEntity : IIdentifiable
+    {
+        Task<TEntity> GetAsync(Guid id);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> BrowseAsync<TQuery>(Expression<Func<TEntity, bool>> predicate, TQuery query);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid id);
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
