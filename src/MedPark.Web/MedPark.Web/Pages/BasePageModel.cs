a98dc117b3b016d6408cea470189a85b225472fb
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-07-02 18:02:24 +0200

    -added base page model for web

---
+++ src/MedPark.Web/MedPark.Web/Pages/BasePageModel.cs
@@ -0,0 +1,24 @@
+﻿using MedPark.Web.Utils.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MedPark.Web.Pages
+{
+    public class BasePageModel : PageModel
+    {
+        public IIdentityParser<ApplicationUser> _appUserParser;
+        public IHttpClientFactory _httpClient;
+
+
+
+        public BasePageModel(IHttpClientFactory httpClient, IIdentityParser<ApplicationUser> appUserParser)
+        {
+            _httpClient = httpClient;
+            _appUserParser = appUserParser;
+        }
+    }
+}
