9b5e97b1577e5c30727224efa24ff53e55d361fa
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-07-08 18:44:21 +0200

    -added queries for address and acceptableMedicalScheme domains

---
+++ src/MedPark.MedicalPractice/Handlers/MedicalPractice/AcceptedMedicalSchemeQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MedPark.Common;
+using MedPark.Common.Handlers;
+using MedPark.MedicalPractice.Domain;
+using MedPark.MedicalPractice.Dto;
+using MedPark.MedicalPractice.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedPark.MedicalPractice.Handlers.MedicalPractice
+{
+    public class AcceptedMedicalSchemeQueryHandler : IQueryHandler<AcceptedMedicalSchemeQuery, AcceptedMedicalSchemeDTO>
+    {
+        private IMedParkRepository<AcceptedMedicalScheme> _medSchemeRepo { get; }
+        private IMapper _mapper { get; }
+
+        public AcceptedMedicalSchemeQueryHandler(IMedParkRepository<AcceptedMedicalScheme> medSchemeRepo, IMapper mapper)
+        {
+            _medSchemeRepo = medSchemeRepo;
+            _mapper = mapper;
+        }
+
+        public Task<AcceptedMedicalSchemeDTO> HandleAsync(AcceptedMedicalSchemeQuery query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MedPark.MedicalPractice/Handlers/MedicalPractice/AddressQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MedPark.Common;
+using MedPark.Common.Handlers;
+using MedPark.MedicalPractice.Domain;
+using MedPark.MedicalPractice.Dto;
+using MedPark.MedicalPractice.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedPark.MedicalPractice.Handlers.MedicalPractice
+{
+    public class AddressQueryHandler : IQueryHandler<AddressQuery, PracticeAddressDTO>
+    {
+        private IMedParkRepository<Address> _practiceAddressRepo { get; }
+        private IMapper _mapper { get; }
+
+        public AddressQueryHandler(IMedParkRepository<Address> practiceAddressRepo, IMapper mapper)
+        {
+            _practiceAddressRepo = practiceAddressRepo;
+            _mapper = mapper;
+        }
+
+        public async Task<PracticeAddressDTO> HandleAsync(AddressQuery query)
+        {
+            Address addr = await _practiceAddressRepo.GetAsync(query.Id);
+
+            return _mapper.Map<PracticeAddressDTO>(addr);
+        }
+    }
+}
