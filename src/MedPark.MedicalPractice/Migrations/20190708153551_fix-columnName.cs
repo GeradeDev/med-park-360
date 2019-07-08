6bea24ac656a384d9e532e2741b012719de85d0e
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-07-08 17:44:00 +0200

    -rename column name

---
+++ src/MedPark.MedicalPractice/Migrations/20190708153551_fix-columnName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.MedicalPractice.Migrations
+{
+    public partial class fixcolumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PractiveId",
+                table: "Address",
+                newName: "PracticeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PracticeId",
+                table: "Address",
+                newName: "PractiveId");
+        }
+    }
+}
