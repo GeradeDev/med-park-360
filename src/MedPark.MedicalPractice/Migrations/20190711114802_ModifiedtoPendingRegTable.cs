098d58ca1eff4f70b6c7e3f13ffcc5066d899aeb
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-07-11 14:00:08 +0200

    -added migration to add modified column to pend registrations table

---
+++ src/MedPark.MedicalPractice/Migrations/20190711114802_ModifiedtoPendingRegTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.MedicalPractice.Migrations
+{
+    public partial class ModifiedtoPendingRegTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "PendingRegistration",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "PendingRegistration");
+        }
+    }
+}
