06b508a8a4211c0d754e4e2184425c0bd8382a1f
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-07-11 17:53:50 +0200

    -remove institute name from qualification table -update pending registration OTP data type to string

---
+++ src/MedPark.MedicalPractice/Migrations/20190711154839_otpStringDatatype.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.MedicalPractice.Migrations
+{
+    public partial class otpStringDatatype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstituteName",
+                table: "Qualifications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OTP",
+                table: "PendingRegistration",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstituteName",
+                table: "Qualifications",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OTP",
+                table: "PendingRegistration",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
