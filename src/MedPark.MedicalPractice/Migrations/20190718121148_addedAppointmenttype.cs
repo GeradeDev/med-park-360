4e0a206117fac1e80bbdcfb552255aead7c7fbc4
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-07-18 14:13:38 +0200

    -added appointment and app. type tables for specialists on practice service

---
+++ src/MedPark.MedicalPractice/Migrations/20190718121148_addedAppointmenttype.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.MedicalPractice.Migrations
+{
+    public partial class addedAppointmenttype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppointmentAccepted",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    AppointmentTypeId = table.Column<Guid>(nullable: false),
+                    SpecialistId = table.Column<Guid>(nullable: false),
+                    Enabled = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentAccepted", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentType", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppointmentAccepted");
+
+            migrationBuilder.DropTable(
+                name: "AppointmentType");
+        }
+    }
+}
