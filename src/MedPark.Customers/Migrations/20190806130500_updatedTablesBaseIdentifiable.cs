af15b9bbc16d4a4a2b7aed0c3120c702cbf9cf21
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-08-06 15:36:39 +0200

    -added migration after updating customer service domain objects to also use baseIdentifiable

---
+++ src/MedPark.Customers/Migrations/20190806130500_updatedTablesBaseIdentifiable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.CustomersService.Migrations
+{
+    public partial class updatedTablesBaseIdentifiable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Customers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Customers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Address",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Address");
+        }
+    }
+}
