46402bd1d9cad73d7ccc5bd126e68033af9575e6
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-09-06 17:14:11 +0200

    -updated med scheme membership no. data type to string

---
+++ src/MedPark.Bookings/Migrations/20190906150638_updateMedSchemeNoType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.Bookings.Migrations
+{
+    public partial class updateMedSchemeNoType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MedicalAidMembershipNo",
+                table: "Appointment",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "MedicalAidMembershipNo",
+                table: "Appointment",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
