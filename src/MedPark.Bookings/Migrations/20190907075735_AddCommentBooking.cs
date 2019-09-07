67e9d0bd55bf3903017f673907da88d01b885d06
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-09-07 10:07:41 +0200

    -added comment column to appointment table

---
+++ src/MedPark.Bookings/Migrations/20190907075735_AddCommentBooking.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedPark.Bookings.Migrations
+{
+    public partial class AddCommentBooking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Appointment",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Appointment");
+        }
+    }
+}
