b0f0fa0a051446abfa7f016865b46d551f558d25
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2020-01-27 17:08:37 +0200

    -added autoFac and RestEase packaage references to services

--- src/MedPark.Customers/Startup.cs
+++ src/MedPark.Customers/Startup.cs
@@ -27,7 +27,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using RawRabbit.Configuration;
 
 namespace MedPark.CustomersService
 {
