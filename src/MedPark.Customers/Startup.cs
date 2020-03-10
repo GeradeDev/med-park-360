f71583e059295a210dd535468752cc1805e296e0
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2020-03-10 12:19:32 +0200

    -empty commit

--- src/MedPark.Customers/Startup.cs
+++ src/MedPark.Customers/Startup.cs
@@ -70,7 +70,6 @@ namespace MedPark.CustomersService
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
